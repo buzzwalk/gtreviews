11ed0fb9eb15fa3b626f33659b82dd387f3c6b84
nicks <nicksuh17@gmail.com>
2023-09-18 21:38:26 -0400

    gtreviews

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
+import db from "./firebase";
+import { collection, addDoc } from "firebase/firestore"; 
+import { initializeApp } from "firebase/app";
+
+
+// Initialize Firebase
+
+
+// Initialize Cloud Firestore and get a reference to the service
+
+async function handleClick(){ 
+  // Add a new document with a generated id.
+  const docRef = await addDoc(collection(db, "users"), {
+    first: "Ada",
+    last: "Lovelace",
+    born: 1815
+  });
+  console.log("Document written with ID: ", docRef.id);
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className="coolbutton" onClick={handleClick}></button>
     </div>
   );
 }
